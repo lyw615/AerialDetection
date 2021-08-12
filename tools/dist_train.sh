70af0c2fb95ec464b39d68008a9ca75c65038e6b
liuyaowu <931253898@qq.com>
2021-08-12 12:16:21 +0000

    add config

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -1,9 +1,4 @@
-#!/usr/bin/env bash
-
-PYTHON=${PYTHON:-"python"}
-
-CONFIG=$1
-GPUS=$2
-
-$PYTHON -m torch.distributed.launch --nproc_per_node=$GPUS \
-    $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:3}
+  python -m torch.distributed.launch --nproc_per_node=2  --master_port=-29500 \
+    ./train.py --config  /home/data1/yw/github_projects/AerialDetection/configs/DOTA1_5/faster_rcnn_RoITrans_r50_fpn_1x_dota1_5.py  \
+    --launcher pytorch   \
+    --work-dir  /home/data1/yw/data/iobjectspy_out/mmdetection/xf_600_300_sac101 \
